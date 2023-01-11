a89c1a0ec531266ad5d9cb69fe742074388c0c99
FrancisTicgal <francisco@ticgal.com>
2023-01-11 14:21:15 +0100

    Workflows

--- tools/generate_locales.sh
+++ tools/generate_locales.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 NAME=gdrive
 
-
+SCRIPT_DIR=$(dirname $0)
+PLUGIN_DIR=$(readlink -f "$SCRIPT_DIR/..")
 POTFILE=$NAME.pot
-CUR_PATH="`dirname \"$0\"`"
+LOCALES=$PLUGIN_DIR/locales
 
-cd "$CUR_PATH/.."
+echo $SCRIPT_DIR
+echo $PLUGIN_DIR
+echo $POTFILE
+echo $LOCALES
 
-xgettext *.php */*.php -o locales/$POTFILE -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po -k --keyword=__:1,2t --keyword=_x:1,2,3t --keyword=__s:1,2t --keyword=_sx:1,2,3t --keyword=_n:1,2,3,4t --keyword=_sn:1,2t --keyword=_nx:1,2,3t --copyright-holder "TICgal"
 
-cd locales
+cd $PLUGIN_DIR
+# Clean existing file
+rm -f $LOCALES/$POTFILE && touch $LOCALES/$POTFILE > /dev/null
 
-sed -i "s/SOME DESCRIPTIVE TITLE/BORGBASE Glpi Plugin/" $POTFILE
-sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./TICgal, $(date +%Y)/" $POTFILE
-sed -i "s/YEAR/$(date +%Y)/" $POTFILE
+echo Searching PHP files...
+# Append locales from PHP
+xgettext `find -type f -name "*.php"` -o $LOCALES/$POTFILE -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po --join-existing \
+    --keyword=_n:1,2 --keyword=__s --keyword=__ --keyword=_x:1c,2 --keyword=_sx:1c,2 --keyword=_nx:1c,2,3 --keyword=_sn:1,2 --copyright-holder "TICgal" > /dev/null 2>&1
 
-localazy upload
-localazy download
+echo Searching JS files...
+# Append locales from JavaScript
+xgettext `find -type f -name "*.js"` -o $LOCALES/$POTFILE -L JavaScript --add-comments=TRANS --from-code=UTF-8 --force-po --join-existing \
+    --keyword=_n:1,2 --keyword=__ --keyword=_x:1c,2 --keyword=_nx:1c,2,3 \
+    --keyword=i18n._n:1,2 --keyword=i18n.__ --keyword=i18n._p:1c,2 \
+    --keyword=i18n.ngettext:1,2 --keyword=i18n.gettext --keyword=i18n.pgettext:1c,2 --copyright-holder "TICgal" > /dev/null 2>&1
 
-for a in $(ls *.po); do
-	msgmerge -U $a $POTFILE
-	msgfmt $a -o "${a%.*}.mo"
+echo Searching TWIG files...
+# Append locales from Twig templates
+for file in $(find ./templates -type f -name "*.twig")
+do
+    # 1. Convert file content to replace "{{ function(.*) }}" by "<?php function(.*); ?>" and extract strings via std input
+    # 2. Replace "standard input:line_no" by file location in po file comments
+    contents=`cat $file | sed -r "s|\{\{\s*([a-z0-9_]+\(.*\))\s*\}\}|<?php \1; ?>|gi"`
+    cat $file | perl -0pe "s/\{\{(.*?)\}\}/<?php \1; ?>/gism" | xgettext - -o $LOCALES/$POTFILE -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po --join-existing \
+        --keyword=_n:1,2 --keyword=__ --keyword=_x:1c,2 --keyword=_nx:1c,2,3 --copyright-holder "TICgal"
+    sed -i -r "s|standard input:([0-9]+)|`echo $file | sed "s|./||"`:\1|g" $LOCALES/$POTFILE
 done
-rm -f *.po~
